2414262be57cd09b853361ec0f45f2d01488385a
aleenxu <twtxogu@yeah.net>
2020-02-24 13:48:13 +0800

    update map icon show

--- components/GMap/GMap.jsx
+++ components/GMap/GMap.jsx
@@ -22,8 +22,7 @@ class GMap extends React.Component {
     var map = new AMap.Map('container', {
         resizeEnable: true, //是否监控地图容器尺寸变化
         center: [121.4205600000,28.6561100000], //初始化地图中心点
-        mapStyle: "amap://styles/darkblue",
-        // mapStyle: "amap://styles/c3fa565f6171961e94b37c4cc2815ef8",
+        mapStyle: "amap://styles/c3fa565f6171961e94b37c4cc2815ef8",
         zoom:13
       });
   }
