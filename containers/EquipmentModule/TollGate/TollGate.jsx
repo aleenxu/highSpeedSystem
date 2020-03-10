cfd0426d5b0cfc65444ffced4d8c3ef7646e4b14
lichao2121 <1318083077@qq.com>
2020-03-10 18:49:07 +0800

    update

--- containers/EquipmentModule/TollGate/TollGate.jsx
+++ containers/EquipmentModule/TollGate/TollGate.jsx
@@ -1,21 +1,79 @@
+
 import React from 'react'
 import SystemMenu from '../../../components/SystemMenu/SystemMenu'
+import Navigation from '../../../components/Navigation/Navigation'
+import styles from '../EquipmentModule.scss'
+import { Pagination, Input } from 'antd'
+/*         可变情报板 */
 class TollGate extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      
+
     }
   }
   componentDidMount = () => {
 
- }
+  }
+  handlepage = (pageNumber) => {
+    console.log('Page: ', pageNumber);
+  }
   render() {
     return (
-        <div>
-          <SystemMenu />
-          收费站
+      <div>
+        <SystemMenu />
+        <div className={styles.EqMain}>
+          <Navigation />
+          <div className={styles.EqCentent}>
+            <div className={styles.Operation}>
+              <div className={styles.leftItem}>
+                <div><Input /></div>
+                <span className={styles.Button}>搜&nbsp;&nbsp;索</span>
+              </div>
+              <div className={styles.rightItem}>
+                <span className={styles.Button}>新&nbsp;&nbsp;增</span>
+                <span className={styles.Button}>修&nbsp;&nbsp;改</span>
+                <span className={styles.Button}>删&nbsp;&nbsp;除</span>
+              </div>
+            </div>
+            <div className={styles.ContetList}>
+              <div className={styles.listItems}>
+                <div className={styles.listTd} >设备编号</div>
+                <div className={styles.listTd} >设备名称</div>
+                <div className={styles.listTd} >设备厂商</div>
+                <div className={styles.listTd} >型号</div>
+                <div className={styles.listTd} >高速公路</div>
+                <div className={styles.listTd} >桩号</div>
+                <div className={styles.listTd} >经纬度坐标</div>
+                <div className={styles.listTd} >放心</div>
+                <div className={styles.listTd} >IP地址</div>
+                <div className={styles.listTd} >端口号</div>
+              </div>
+              {
+                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => {
+                  return (
+                    <div className={styles.listItems}>
+                      <div className={styles.listTd} ><span className={styles.roadName}>11111</span></div>
+                      <div className={styles.listTd} ><span className={styles.roadName}>11111</span></div>
+                      <div className={styles.listTd} ><span className={styles.roadName}>11111</span></div>
+                      <div className={styles.listTd} ><span className={styles.roadName}>11111</span></div>
+                      <div className={styles.listTd} ><span className={styles.roadName}>11111</span></div>
+                      <div className={styles.listTd} ><span className={styles.roadName}>11111</span></div>
+                      <div className={styles.listTd} ><span className={styles.roadName}>11111</span></div>
+                      <div className={styles.listTd} ><span className={styles.roadName}>11111</span></div>
+                      <div className={styles.listTd} ><span className={styles.roadName}>11111</span></div>
+                      <div className={styles.listTd} ><span className={styles.roadName}>11111</span></div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+            <div className={styles.Footer}>
+              <div className={styles.page}><span className={styles.count}>当前共{150}条，每页显示10条</span><Pagination showQuickJumper defaultCurrent={2} total={500} onChange={this.handlepage} /></div>
+            </div>
+          </div>
         </div>
+      </div>
     )
   }
 }
