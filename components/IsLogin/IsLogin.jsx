d15ca9266f588f0ed5d60e98daf87503d1d2832c
lichao2121 <1318083077@qq.com>
2020-03-27 18:30:55 +0800

    update

--- components/IsLogin/IsLogin.jsx
+++ components/IsLogin/IsLogin.jsx
@@ -1,27 +1,41 @@
 import React from 'react'
 import { Menu, Dropdown, Icon } from 'antd';
 import styles from './IsLogin.scss'
-const menu = (
-    <Menu>
-      <Menu.Item key="0">用户：admin</Menu.Item>
-      <Menu.Item key="1">退出系统</Menu.Item>
-    </Menu>
-  );
+import {message} from 'antd'
+import getResponseDatas from '../../plugs/HttpData/getResponseData'
+
 class IsLogin extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       
     }
+    this.menu = (
+      <Menu>
+        <Menu.Item key="0">用户：admin</Menu.Item>
+        <Menu.Item key="1" onClick={this.handleLogout}>退出系统</Menu.Item>
+      </Menu>
+    );
+    this.logoutUrl = '/control/sys/user/logout'
   }
   componentDidMount = () => {
     
   }
-
+  handleLogout = () => {
+    getResponseDatas('post', this.logoutUrl).then((res) => {
+      const { code, msg } = res.data
+      if (code === 0) {
+        localStorage.clear()
+        this.props.history.push('/login')
+      } else {
+        message.warning(msg)
+      }
+    })
+  }
   render() {
     return (
         <div className={styles.isLoginBox}>
-            <Dropdown overlay={menu}>
+            <Dropdown overlay={this.menu}>
                 <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
                 <Icon className={styles.imgHead} type="user" /> <Icon className={styles.arrow} type="down" />
                 </a>
